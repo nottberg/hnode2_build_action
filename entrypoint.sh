b610e841cbe9f25304e7582fd426c29dd206ff06
Curtis Nottberg <curtisn@comcast.net>
2020-04-15 12:14:37 -0600

    Add initial build commands.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,14 @@
 echo $GITHUB_WORKSPACE
 rootls=$(ls -lah)
 echo "Root $rootls"
+
 cd ${GITHUB_WORKSPACE}
 libls=$(ls -lah)
 echo "Lib $libls"
+mkdir build
+cd build
+cmake ..
+make package
 
 
 echo "Hello $1"
